c17ba8267e6e900aa78576e76f990e2d1f7f9639
Git Privremeni <git.privremeni.email@protonmail.com>
2021-08-21 11:03:05 +0200

    [refactor] refactor single linked list

--- 19_Singly_Linked_Lists/112_singly_linked_list.js
+++ 19_Singly_Linked_Lists/112_singly_linked_list.js
@@ -14,6 +14,7 @@ class SinglyLinkedList{
 
     push(val){
         let newNode = new Node(val);
+
         if(!this.head){
             this.head = newNode;
             this.tail = this.head;
@@ -21,68 +22,85 @@ class SinglyLinkedList{
             this.tail.next = newNode;
             this.tail = newNode;
         }
+
         this.length++;
         return this;
     }
 
     pop(){
         if(!this.head) return undefined;
+
         let current = this.head;
         let newTail = current;
+
         while(current.next){
             newTail = current;
             current = current.next;
         }
+        
         this.tail = newTail;
         this.tail.next = null;
         this.length--;
+
         if(this.length === 0){
             this.head = null;
             this.tail = null;
         }
+
         return current;
     }
 
     shift(){
         if(!this.head) return undefined;
         let currentHead = this.head;
+
         this.head = currentHead.next;
         this.length--;
+
         if(this.length === 0){
             this.tail = null;
         }
+
         return currentHead;
     }
 
     unshift(val){
         let newNode = new Node(val);
+
         if(!this.head) {
             this.head = newNode;
             this.tail = this.head;
         }
+
         newNode.next = this.head;
         this.head = newNode;
         this.length++;
+
         return this;
     }
 
     get(index){
         if(index < 0 || index >= this.length) return null;
+
         let counter = 0;
         let current = this.head;
+
         while(counter !== index){
             current = current.next;
             counter++;
         }
+
         return current;
     }
 
     set(index, val){
         let foundNode = this.get(index);
+
         if(foundNode){
             foundNode.val = val;
             return true;
         }
+
         return false;
     }
 
@@ -94,9 +112,11 @@ class SinglyLinkedList{
         let newNode = new Node(val);
         let prev = this.get(index - 1);
         let temp = prev.next;
+
         prev.next = newNode;
         newNode.next = temp;
         this.length++;
+
         return true;
     }
 
@@ -104,10 +124,13 @@ class SinglyLinkedList{
         if(index < 0 || index >= this.length) return undefined;
         if(index === 0) return this.shift();
         if(index === this.length - 1) return this.pop();
+
         let previousNode = this.get(index - 1);
         let removed = previousNode.next;
+
         previousNode.next = removed.next;
         this.length--;
+
         return removed;
     }
 
@@ -115,24 +138,29 @@ class SinglyLinkedList{
       let node = this.head;
       this.head = this.tail;
       this.tail = node;
+
       let next;
       let prev = null;
+
       for(let i = 0; i < this.length; i++){
         next = node.next;
         node.next = prev;
         prev = node;
         node = next;
       }
+
       return this;
     }
     
     print(){
         let arr = [];
         let current = this.head
+
         while(current){
             arr.push(current.val)
             current = current.next
         }
+        
         console.log(arr);
     }
 }
