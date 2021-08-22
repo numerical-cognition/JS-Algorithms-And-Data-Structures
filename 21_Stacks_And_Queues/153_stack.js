78e947be97fd8b56c86b29bd108a1f053cba11f4
Git Privremeni <git.privremeni.email@protonmail.com>
2021-08-22 10:47:04 +0200

    [feature] add a stack and a queue data structure

---
+++ 21_Stacks_And_Queues/153_stack.js
@@ -0,0 +1,43 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+
+        return ++this.size;
+    }
+
+    pop() {
+        if (!this.first) return null;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+
+        let temp = this.first;
+
+        this.first = this.first.next;
+        this.size--;
+
+        return temp.value;
+    }
+}
